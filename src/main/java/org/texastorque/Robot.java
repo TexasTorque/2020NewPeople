f8948119320d6544f2e1a8ba3cee1c30523e2416
saddy <jrp30074@gmail.com>
2020-11-13 16:45:49 -0600

    Testing getting an instance

--- src/main/java/org/texastorque/Robot.java
+++ src/main/java/org/texastorque/Robot.java
@@ -25,6 +25,8 @@ public class Robot extends TimedRobot {
   private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
+  
+  private DriveBase driveBase = DriveBase.getInstance();
 
   /**
    * This function is run when the robot is first started up and should be
@@ -35,7 +37,6 @@ public class Robot extends TimedRobot {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
-    DriveBase driveBase = new DriveBase();
   }
 
   /**
@@ -73,6 +74,7 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void autonomousPeriodic() {
+    driveBase.run();
     switch (m_autoSelected) {
       case kCustomAuto:
         // Put custom auto code here
--- src/main/java/org/texastorque/subsystems/DriveBase.java
+++ src/main/java/org/texastorque/subsystems/DriveBase.java
@@ -1,25 +1,34 @@
 package org.texastorque.subsystems;
 
+import org.texastorque.torquelib.component.TorqueSparkMax;
 import org.texastorque.torquelib.util.GenericController;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class DriveBase {
-    GenericController controller = new GenericController(0, 0.1);
+    private static volatile DriveBase instance;
+    private final int port_left = 0;
+    private final int port_right = 1;
 
+    GenericController controller = new GenericController(0, 0.1);
+    TorqueSparkMax left = new TorqueSparkMax(port_left);
+    TorqueSparkMax right = new TorqueSparkMax(port_right);
+    
     public DriveBase(){
         SmartDashboard.putString("test", "Instantiated Drivebase");        
-        run();
     }
 
     public void run() {
-        try {
-            while(true) {
-                System.out.println(controller.getLeftYAxis());
-                Thread.sleep(100);
+        System.out.println(controller.getLeftYAxis());
+    }
+
+    public static DriveBase getInstance(){
+        if (instance == null){
+            synchronized(DriveBase.class){
+                if (instance == null)
+                    instance = new DriveBase();
             }
-        } catch(InterruptedException e) {
-            System.out.println("Thread interrupted: "+e);
         }
+        return instance;
     }
 }
