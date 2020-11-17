7dae5b03bab29b42025b58a529d4bc540e63d971
saddy <jrp30074@gmail.com>
2020-11-17 16:24:54 -0600

    ´TorqueIterative´

--- src/main/java/org/texastorque/Robot.java
+++ src/main/java/org/texastorque/Robot.java
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import org.texastorque.subsystems.DriveBase;
+import org.texastorque.torquelib.base.TorqueIterative;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -20,10 +21,9 @@ import org.texastorque.subsystems.DriveBase;
  * creating this project, you must also update the build.gradle file in the
  * project.
  */
-public class Robot extends TimedRobot {
+public class Robot extends TorqueIterative {
   private static final String kDefaultAuto = "Default";
   private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
   
   private DriveBase driveBase = DriveBase.getInstance();
@@ -40,53 +40,6 @@ public class Robot extends TimedRobot {
   }
 
   /**
-   * This function is called every robot packet, no matter the mode. Use
-   * this for items like diagnostics that you want ran during disabled,
-   * autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before
-   * LiveWindow and SmartDashboard integrated updating.
-   */
-  @Override
-  public void robotPeriodic() {
-  }
-
-  /**
-   * This autonomous (along with the chooser code above) shows how to select
-   * between different autonomous modes using the dashboard. The sendable
-   * chooser code works with the Java SmartDashboard. If you prefer the
-   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-   * getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to
-   * the switch structure below with additional strings. If using the
-   * SendableChooser make sure to add them to the chooser code above as well.
-   */
-  @Override
-  public void autonomousInit() {
-    m_autoSelected = m_chooser.getSelected();
-    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
-    System.out.println("Auto selected: " + m_autoSelected);
-  }
-
-  /**
-   * This function is called periodically during autonomous.
-   */
-  @Override
-  public void autonomousPeriodic() {
-    driveBase.run();
-    switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
-    }
-  }
-
-  /**
    * This function is called once when teleop is enabled.
    */
   @Override
@@ -100,6 +53,10 @@ public class Robot extends TimedRobot {
   public void teleopPeriodic() {
   }
 
+  @Override
+  public void teleopContinuous() {
+    driveBase.run();
+  }
   /**
    * This function is called once when the robot is disabled.
    */
@@ -126,5 +83,10 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void testPeriodic() {
+  }
+
+  @Override
+  public void endCompetition() {
+    
   }
 }
