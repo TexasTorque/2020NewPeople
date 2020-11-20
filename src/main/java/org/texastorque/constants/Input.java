bc323914072c546434c82b3a5d34c06a1c6f966b
saddy <jrp30074@gmail.com>
2020-11-20 17:19:55 -0600

    Better tuned drive, Input constants

---
+++ src/main/java/org/texastorque/constants/Input.java
@@ -0,0 +1,11 @@
+package org.texastorque.constants;
+
+public class Input {
+    // DriveBase
+    public static final int DB_LEFT_1 = 0;
+    public static final int DB_LEFT_2 = 1;
+    public static final int DB_LEFT_3 = 2;
+    public static final int DB_RIGHT_1 = 3;
+    public static final int DB_RIGHT_2 = 4;
+    public static final int DB_RIGHT_3 = 5;
+}
--- src/main/java/org/texastorque/subsystems/DriveBase.java
+++ src/main/java/org/texastorque/subsystems/DriveBase.java
@@ -1,5 +1,6 @@
 package org.texastorque.subsystems;
 
+import org.texastorque.constants.Input;
 import org.texastorque.torquelib.util.GenericController;
 
 import edu.wpi.first.wpilibj.PWMSparkMax;
@@ -7,31 +8,25 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class DriveBase {
     private static volatile DriveBase instance;
-    private final int port_left_1 = 0;
-    private final int port_left_2 = 1;
-    private final int port_left_3 = 2;
-    private final int port_right_1 = 3;
-    private final int port_right_2 = 4;
-    private final int port_right_3 = 5;
     private double leftSpeed = 0.0;
     private double rightSpeed = 0.0;
 
     GenericController controller = new GenericController(0, 0.1);
-    PWMSparkMax left_1 = new PWMSparkMax(port_left_1);
-    PWMSparkMax left_2 = new PWMSparkMax(port_left_2);
-    PWMSparkMax left_3 = new PWMSparkMax(port_left_3);
-    PWMSparkMax right_1 = new PWMSparkMax(port_right_1);
-    PWMSparkMax right_2 = new PWMSparkMax(port_right_2);
-    PWMSparkMax right_3 = new PWMSparkMax(port_right_3);
+    PWMSparkMax left_1 = new PWMSparkMax(Input.DB_LEFT_1);
+    PWMSparkMax left_2 = new PWMSparkMax(Input.DB_LEFT_2);
+    PWMSparkMax left_3 = new PWMSparkMax(Input.DB_LEFT_3);
+    PWMSparkMax right_1 = new PWMSparkMax(Input.DB_RIGHT_1);
+    PWMSparkMax right_2 = new PWMSparkMax(Input.DB_RIGHT_2);
+    PWMSparkMax right_3 = new PWMSparkMax(Input.DB_RIGHT_3);
     
     public DriveBase(){
         SmartDashboard.putString("test", "Instantiated Drivebase");        
     }
 
     public void run() {
-        double leftRight = controller.getRightXAxis();
-        leftSpeed =.2*(controller.getLeftYAxis() - 0.4 * Math.pow(leftRight, 4) * Math.signum(leftRight));
-        rightSpeed = .2*(-controller.getLeftYAxis() - 0.4 * Math.pow(leftRight, 4) * Math.signum(leftRight));
+        double leftRight = -controller.getRightXAxis();
+        leftSpeed =.8*(-controller.getLeftYAxis() - 0.8 * Math.pow(leftRight, 4) * Math.signum(leftRight));
+        rightSpeed = .8*(controller.getLeftYAxis() - 0.8 * Math.pow(leftRight, 4) * Math.signum(leftRight));
         left_1.set(leftSpeed);
         left_2.set(leftSpeed);
         left_3.set(leftSpeed);
